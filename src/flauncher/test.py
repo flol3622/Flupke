7a3b985847f42d1cc55cf6d752271498c7b932b5
Philippe Soubrier <philippe.soubrier@ugent.be>
2025-09-10 19:38:43 +0200

    better test

--- src/flauncher/test.py
+++ src/flauncher/test.py
@@ -1,5 +1,5 @@
-def print_message(message):
-    print(message)
+def print_message():
+    print("Hello from Flupke!")
     # wait for user to press Enter
     input("Press Enter to continue...")
     
